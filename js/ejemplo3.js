b098d12481f341992147ca2e9d18625a8b535676
Mynor Escobar <mynorescobarefpem@gmail.com>
2026-04-24 09:30:58 -0600

    ejemplos 6 de abril

---
+++ js/ejemplo3.js
@@ -0,0 +1,6 @@
+function mostrarMensaje(){
+    document.getElementById('mensaje').innerHTML='hola, presionaste el boton';
+}
+function cambiarMensaje(){
+    document.getElementById('mensaje').innerHTML="mensaje cambiado";
+}
